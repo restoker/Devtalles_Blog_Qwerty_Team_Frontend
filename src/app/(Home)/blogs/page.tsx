e3b0c0a0e40d1f018378492657163daca9393fd7
Alex <past6867@gmail.com>
2025-09-16 05:42:32 -0500

    Blog 1

--- src/app/(Home)/blogs/page.tsx
+++ src/app/(Home)/blogs/page.tsx
@@ -51,7 +51,7 @@ const posts = [
     {
         id: 1,
         title: 'Boost your conversion rate',
-        href: '#',
+        href: '/blogs/blogsPredeterminado/Blog1',
         description:
             'Illo sint voluptas. Error voluptates culpa eligendi. Hic vel totam vitae illo. Non aliquid explicabo necessitatibus unde. Sed exercitationem placeat consectetur nulla deserunt vel. Iusto corrupti dicta.',
         imageUrl:
