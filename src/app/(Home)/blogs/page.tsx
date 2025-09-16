2f51b1b0ecef9cf2f172e373808e95332cc44215
Alex <past6867@gmail.com>
2025-09-16 06:03:46 -0500

    Blog4

--- src/app/(Home)/blogs/page.tsx
+++ src/app/(Home)/blogs/page.tsx
@@ -106,7 +106,7 @@ const posts = [
     {
         id: 4,
         title: 'Boost your conversion rate',
-        href: '#',
+        href: '/blogs/sql-vs-nosql',
         description: 'Illo sint voluptas. Error voluptates culpa eligendi. Hic vel totam vitae illo. Non aliquid explicabo necessitatibus unde. Sed exercitationem placeat consectetur nulla deserunt vel. Iusto corrupti dicta.',
         imageUrl:
             '/img/img4.webp',
